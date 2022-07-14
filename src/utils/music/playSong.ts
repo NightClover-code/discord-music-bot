0876c4ec4995a031c68ea6485c35ea59a5af8170
Achraf Elmouhib <elmouhib2003achraf@gmail.com>
2022-07-14 01:28:02 +0100

    feat: will introduce playlists

--- src/utils/music/playSong.ts
+++ src/utils/music/playSong.ts
@@ -14,23 +14,9 @@ export const playSong = async (
   if (!channel) return message.reply('You need to join a voice channel.');
 
   if (!_args.length)
-    return message.reply('You need to provide a URL or a search term.');
-
-  const search = _args.join(' ');
-  let res;
-
-  try {
-    res = await client.manager.search(search, message.author);
-
-    if (res.loadType === 'LOAD_FAILED') throw res.exception;
-    else if (res.loadType === 'PLAYLIST_LOADED')
-      throw { message: 'Playlists are not supported with this command.' };
-  } catch (err) {
-    return message.reply(`There was an error while searching!`);
-  }
-
-  if (res.loadType === 'NO_MATCHES')
-    return message.reply('There was no tracks found with that query.');
+    return message.reply(
+      'You need to provide a URL or a search term, or a playlist.'
+    );
 
   const player = client.manager.create({
     guild: guildId,
@@ -38,10 +24,54 @@ export const playSong = async (
     textChannel: channel.id,
   });
 
-  player.connect();
-  player.queue.add(res.tracks[0]);
+  const search = _args.join(' ');
+  let res, tracks;
 
-  if (!player.playing && !player.paused && !player.queue.size) player.play();
+  const songs = ['intervals 5 htp', 'james franco polyphia'];
 
-  return message.reply(`${res.tracks[0].title} is added to queue!`);
+  try {
+    if (search.startsWith('playlist:')) {
+      message.reply(`Loading tracks...`);
+
+      tracks = await Promise.all(
+        songs.map(
+          async song =>
+            (
+              await client.manager.search(song, message.author)
+            ).tracks[0]
+        )
+      );
+      if (player.state !== 'CONNECTED') player.connect();
+
+      player.queue.add(tracks);
+      if (
+        !player.playing &&
+        !player.paused &&
+        player.queue.totalSize === tracks.length
+      )
+        player.play();
+
+      console.log(tracks);
+
+      return message.reply(`Multiple tracks added to queue!`);
+    } else {
+      res = await client.manager.search(search, message.author);
+
+      if (res.loadType === 'NO_MATCHES')
+        return message.reply('There was no tracks found with that query.');
+      else if (res.loadType === 'LOAD_FAILED') throw res.exception;
+      else if (res.loadType === 'PLAYLIST_LOADED')
+        throw { message: 'Playlists are not supported with this command.' };
+
+      player.connect();
+      player.queue.add(res.tracks[0]);
+
+      if (!player.playing && !player.paused && !player.queue.size)
+        player.play();
+
+      return message.reply(`${res.tracks[0].title} is added to queue!`);
+    }
+  } catch (err) {
+    return message.reply(`There was an error while searching!`);
+  }
 };
